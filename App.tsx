e7256b640d83b67894744e99e9791e2cb7a3ce14
olivemaygroup <ed@olivemaygroup.com>
2024-01-19 14:51:22 +0000

    FEAT: npm i and api keys

--- App.tsx
+++ App.tsx
@@ -6,9 +6,7 @@ import { ArtistProvider } from './src/contexts/ArtistContext';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from '@routes/index';
-
 import { THEME } from './src/theme'
-
 import { Loading } from '@components/Loading';
 
 
@@ -22,13 +20,13 @@ export default function App() {
       <ArtistProvider>
 
 
-        <StatusBar
+        {/* <StatusBar
           barStyle='light-content'
           backgroundColor='transparent'
           translucent
         />
         {fontsLoaded ? <Routes /> : <Loading /> }
-
+ */}
 
       </ArtistProvider>
 
